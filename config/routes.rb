2e67cb4f87c68543cd07171431b43125fc8ab9b0
Tatsuya Kyushima <kyu08@TatsuyanoMacBook-Air.local>
2019-06-30 15:28:52 +0900

    editing users_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-    get '/' => "home#top"
-
+    get 'signup' => "users#new"
+    get "analysis" => "analysis#analysis"
+    get "input" => "input#input"
+    post "users/:id/update" => "users#update"
+    get "users/:id/edit" => "users#edit"
+    post "users/create" => "users#create"
+    get "users/:id" => "users#show"
+    get "signin" => "users#login"
+    
+    get "/" => "home#top"
+    get "about" => "home#about"
 end
 
 
